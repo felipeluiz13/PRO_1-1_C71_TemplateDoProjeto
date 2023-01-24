724200cc2aa2f2ab42ee8f56039ac0c3fab176a9
felipeluiz13 <97695989+felipeluiz13@users.noreply.github.com>
2023-01-24 16:53:26 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,14 @@ require("@firebase/firestore");
 
 var firebaseConfig = {
   //Cole sua Configuração do Firebase aqui
-  
+  apiKey: "AIzaSyAUwTth--AqmNmoXQ26D1LHMIRBIP_5m3E",
+  authDomain: "bicicletas-a132d.firebaseapp.com",
+  projectId: "bicicletas-a132d",
+  storageBucket: "bicicletas-a132d.appspot.com",
+  messagingSenderId: "834536499979",
+  appId: "1:834536499979:web:a6218327a0843fd136d00e"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 
